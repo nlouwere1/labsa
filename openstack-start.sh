765c560ed3d2df791df1b65ba07b2892cfffb0f7
nlouwere1 <nlouwere@me.com>
2017-04-15 06:52:21 +0200

    Update openstack-start.sh

--- openstack-start.sh
+++ openstack-start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# this file can be pulled git clone https://github.com/nlouwere1/labsa.git
+# first section is to force a speciffic mirror in yum had issues that it takes a repo that was down
+
+yum install -y nfs-utils libnfsidmap
+mkdir /nfs
+mount -o rw -t nfs nas:/export/openstack /nfs
+/bin/cp /nfs/repo/CentOS-Base.repo /etc/yum.repos.d/.
 yum update -y
 yum install -y epel-release
 yum install -y https://rdo.fedorapeople.org/rdo-release.rpm
@@ -9,6 +16,7 @@ mkdir /cinder
 chmod 777 /cinder
 chown root.wheel/cinder
 echo "/cinder    10.0.0.0/8(rw,sync,no_root_squash,no_all_squash,insecure)" >> /etc/exports
+echo "10.0.1.1:/export/openstack    /nfs   nfs defaults 0 0" >> /etc/fstab
 yum install -y openstack-packstack
 yum install -y openstack-tools
 tee /etc/environment<<-'EOF'
