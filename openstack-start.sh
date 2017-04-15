7207633d39a7324f99b28f321420e6e917393346
nlouwere1 <nlouwere@me.com>
2017-04-15 07:20:14 +0200

    Update openstack-start.sh

--- openstack-start.sh
+++ openstack-start.sh
@@ -17,6 +17,7 @@ chmod 777 /cinder
 chown root.wheel/cinder
 echo "/cinder    10.0.0.0/8(rw,sync,no_root_squash,no_all_squash,insecure)" >> /etc/exports
 echo "10.0.1.1:/export/openstack    /nfs   nfs defaults 0 0" >> /etc/fstab
+/bin/cp /nfs/repo2/* /etc/yum.repos.d/.
 yum install -y openstack-packstack
 yum install -y openstack-tools
 tee /etc/environment<<-'EOF'
@@ -32,5 +33,9 @@ systemctl start network
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 echo ''
 echo 'you are now ready to run packstack'
+echo ''
+echo 'disbled selinx,firewall,networkmanager enabled network and lanugae support'
+echo ''
+echo 'please configure second interfave to manual and NM_CONTROLLED=no'
 echo ''
 echo 'packstack --gen-answer-file=answer.txt --default-password=cisco --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:ens256 --os-neutron-ml2-type-drivers=vxlan,flat,vlan --cinder-volumes-create=n --nagios-install=n --os-swift-install=n --os-cinder-install=y --cinder-nfs-mounts=10.0.1.21:/cinder --cinder-backend=nfs --os-neutron-lbaas-install=y --os-heat-install=y'
